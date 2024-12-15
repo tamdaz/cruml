3cf0586252f65fb7b7931de254ba46f85e6c34be
tamdaz <tamda.zohir.pro@gmail.com>
2024-12-16 00:58:54 +0100

    Add the `Cruml::run` method that allows to proceed to the creation of the UML class diagram.

--- src/cruml.cr
+++ src/cruml.cr
@@ -1,10 +1,37 @@
 require "./annotations"
-require "./generators/**"
+require "./diagram_render"
+require "./reflector"
 require "./../examples/**"
+
+::CRUML_FILTER_PREFIX = "Example"
 
 # TODO: Write documentation for `Cruml`
 module Cruml
   VERSION = "0.1.0"
 
-  Cruml::ClassReflector.list_classes_and_methods
+  alias ClassArray = Array(String)
+  alias InstanceVarsArray = Array(Tuple(String, String))
+  alias MethodsArray = Array(Tuple(Symbol, String, String))
+
+  # Proceed to the UML diagram generation.
+  def self.run : Nil
+    reflected_classes = Cruml::Reflector.reflect_classes
+    reflected_instance_vars = Cruml::Reflector.reflect_instance_vars
+    reflected_methods = Cruml::Reflector.reflect_methods
+
+    pp! reflected_classes.size
+    pp! reflected_instance_vars.size
+    pp! reflected_methods.size
+
+    if reflected_classes.empty?
+      puts "\e[31mCannot generate a UML diagram because there's no selected classes. Abort.\e[0m"
+      exit 1
+    end
+
+    diagram_render = Cruml::DiagramRender.new(Path["out"])
+    diagram_render.generate(reflected_classes, reflected_instance_vars, reflected_methods)
+    diagram_render.save
+  end
+
+  run
 end
