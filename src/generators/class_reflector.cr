e2a035d028b5ff51755a1b2c20ca21b69d4e4006
tamdaz <tamda.zohir.pro@gmail.com>
2024-12-15 20:07:02 +0100

    Rename the `ClassReflector` file name to "snake_case".

--- src/generators/class_reflector.cr
+++ src/generators/class_reflector.cr
@@ -1,3 +1,9 @@
+# ```text
+# - Array of classes
+#  - Classes (name only)
+#   - Instance vars (name and type)
+#   - Methods (scope, name and return type)
+# ```
 class Cruml::ClassReflector
   # Get all specified classes.
   def self.reflect_classes : Array(String)
@@ -19,9 +25,7 @@ class Cruml::ClassReflector
       {% if child_class.name.starts_with?("Example") && !(child_class.name.ends_with?(".class")) %}
         {% unless child_class.resolve.instance_vars.empty? %}
           {% for instance_var in child_class.resolve.instance_vars %}
-            {% stored_instance_var = {
-                 instance_var.name.stringify, instance_var.type.stringify,
-               } %}
+            {% stored_instance_var = { instance_var.name.stringify, instance_var.type.stringify } %}
             reflected_instance_vars << [ {{ stored_instance_var }} ]
           {% end %}
         {% end %}
@@ -51,28 +55,6 @@ class Cruml::ClassReflector
       {% end %}
     {% end %}
     reflected_methods
-  end
-
-  # List available methods for each class.
-  def self.list_classes_and_methods : Nil
-    reflected_classes = Cruml::ClassReflector.reflect_classes
-    reflected_instance_vars = Cruml::ClassReflector.reflect_instance_vars
-    reflected_methods = Cruml::ClassReflector.reflect_methods
-    i = 0
-
-    until i == reflect_classes.size
-      puts "\e[4m#{reflect_classes[i]}\e[0m"
-      reflected_instance_vars[i].each do |iv|
-        # Scope is always set as "private" for instance vars.
-        puts "\e[1m- #{iv[0]} : #{iv[1]}\e[0m"
-      end
-      puts "-" * 20
-      reflected_methods[i].each do |m|
-        puts "\e[1m#{get_scope(m[-3])} #{m[-2]} : #{m[-1].empty? ? "Nil" : m[-1]}\e[0m"
-      end
-      puts "\n"
-      i += 1
-    end
   end
 
   # Return the scope with special character.
