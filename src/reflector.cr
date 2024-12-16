225f8637204a370ab99e1df39e9864add17de9a6
tamdaz <tamda.zohir.pro@gmail.com>
2024-12-16 01:00:37 +0100

    Add a constant that enables to select the classes.

--- src/reflector.cr
+++ src/reflector.cr
@@ -10,8 +10,10 @@ class Cruml::Reflector
     reflected_class = [] of String
     {% unless Object.all_subclasses.size == 0 %}
       {% for subclass in Object.all_subclasses %}
-        {% if subclass.name.starts_with?("Example") && !(subclass.name.ends_with?(".class")) %}
-          reflected_class << {{ subclass.name.stringify }}
+        {% if subclass.name.starts_with?(::CRUML_FILTER_PREFIX) && !(subclass.name.ends_with?(".class")) %}
+          {% unless subclass.name.ends_with?(":Module") %}
+            reflected_class << {{ subclass.name.stringify }}
+          {% end %}
         {% end %}
       {% end %}
     {% end %}
@@ -22,7 +24,7 @@ class Cruml::Reflector
   def self.reflect_instance_vars : Array(InstanceVarsArray)
     reflected_instance_vars = [] of InstanceVarsArray
     {% for subclass in Object.all_subclasses %}
-      {% if subclass.name.starts_with?("Example") && !(subclass.name.ends_with?(".class")) %}
+      {% if subclass.name.starts_with?(::CRUML_FILTER_PREFIX) && !(subclass.name.ends_with?(".class")) %}
         {% unless subclass.resolve.instance_vars.empty? %}
           {% for instance_var in subclass.resolve.instance_vars %}
             {% stored_instance_var = { instance_var.name.stringify, instance_var.type.stringify } %}
@@ -38,7 +40,7 @@ class Cruml::Reflector
   def self.reflect_methods : Array(MethodsArray)
     reflected_methods = [] of MethodsArray
     {% for subclass in Object.all_subclasses %}
-      {% if subclass.name.starts_with?("Example") && !(subclass.name.ends_with?(".class")) %}
+      {% if subclass.name.starts_with?(::CRUML_FILTER_PREFIX) && !(subclass.name.ends_with?(".class")) %}
         {% unless subclass.methods.size == 0 %}
           # Store for each method : scope, name and return type.
           {% stored_methods = [] of Tuple(Symbol, String, String) %}
