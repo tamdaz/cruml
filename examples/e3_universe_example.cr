c712425046a7d35ee2c519953befd5bec8cef14e
tamdaz <tamda.zohir.pro@gmail.com>
2024-12-16 22:49:05 +0100

    Add a new example E3 : solar system hierarchy.

---
+++ examples/e3_universe_example.cr
@@ -0,0 +1,21 @@
+class Example::E3::Universe
+  @size : Int32
+
+  def initialize(@size : Int32); end
+end
+
+class Example::E3::Galaxy < Example::E3::Universe
+  def initialize(@size : Int32); end
+end
+
+class Example::E3::BlackHole < Example::E3::Galaxy
+  def initialize(@size : Int32); end
+end
+
+class Example::E3::Planet < Example::E3::Galaxy
+  def initialize(@size : Int32); end
+end
+
+class Example::E3::Star < Example::E3::Galaxy
+  def initialize(@size : Int32); end
+end
--- examples/run_examples.cr
+++ examples/run_examples.cr
@@ -1,5 +1,7 @@
 require "./../src/cruml"
 require "./e1_person_example"
+require "./e2_organization_example"
+require "./e3_universe_example"
 
 ::CRUML_FILTER_PREFIX = "Cruml"
 
