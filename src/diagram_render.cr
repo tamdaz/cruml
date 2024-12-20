711e646b5d00cc3c0b991458ad3719ed6e8eae22
tamdaz <tamda.zohir.pro@gmail.com>
2024-12-20 22:54:04 +0100

    Refactor `diagram_render.cr`.

--- src/diagram_render.cr
+++ src/diagram_render.cr
@@ -9,32 +9,74 @@ class Cruml::DiagramRender
     reflected_instance_vars : Array(InstanceVarsArray),
     reflected_methods : Array(MethodsArray)
   ) : Nil
-    reflected_link_subclasses.each do |class_name, subclass_name|
-      @code += "#{class_name.split("::")[-1]} --|> #{subclass_name.split("::")[-1]}\n"
-    end
+    add_links(reflected_link_subclasses)
+
     i = 0
     until i == reflected_classes.size
-      class_name = reflected_classes[i].split("::")[-1]
+      add_classes(reflected_classes, i)
+      add_instance_vars(reflected_instance_vars, i)
+
+      unless reflected_methods.size == 0
+        reflected_methods[i].each do |method|
+          scope = case method[-3]
+                  when :public    then "+"
+                  when :protected then "#"
+                  when :private   then "-"
+                  end
+          name = method[-2]
+          return_type = method[-1]
+          @code += "  #{scope}#{name}() #{return_type}\n"
+        end
+      end
+
+      @code += "}\n\n"
+      i += 1
+    end
+
+    set_diagram_colors
+  end
+
+  private def add_classes(reflected_classes : ClassArray, i : Int32) : Nil
+    class_name = reflected_classes[i][0].split("::")[-1]
+
+    case reflected_classes[i][1]
+    when :interface
+      @code += "&lt;&lt;interface&gt;&gt; #{class_name}\n"
+      @code += "class #{class_name}:::interface {\n"
+    when :abstract
+      @code += "&lt;&lt;abstract&gt;&gt; #{class_name}\n"
       @code += "class #{class_name} {\n"
+    when :class
+      @code += "class #{class_name} {\n"
+    end
+  end
+
+  private def add_links(reflected_link_subclasses : LinkSubClassArray) : Nil
+    reflected_link_subclasses.each do |class_name, subclass_name, class_type|
+      case class_type
+      when :interface
+        @code += "#{class_name.split("::")[-1]} ..|> #{subclass_name.split("::")[-1]}\n"
+      when :abstract
+        @code += "#{class_name.split("::")[-1]} --|> #{subclass_name.split("::")[-1]}\n"
+      when :class
+        @code += "#{class_name.split("::")[-1]} --|> #{subclass_name.split("::")[-1]}\n"
+      end
+    end
+  end
+
+  private def add_instance_vars(reflected_instance_vars : Array(InstanceVarsArray), i : Int32) : Nil
+    unless reflected_instance_vars.size == 0
       reflected_instance_vars[i].each do |instance_var|
         name = instance_var[-2]
         type = instance_var[-1]
         @code += "  -#{name} : #{type}\n"
       end
-      reflected_methods[i].each do |method|
-        scope = case method[-3]
-                when :public    then "+"
-                when :protected then "#"
-                when :private   then "-"
-                end
-        name = method[-2]
-        return_type = method[-1]
-        @code += "  #{scope}#{name}() #{return_type}\n"
-      end
-      @code += "}\n\n"
-      i += 1
     end
-    @code += "classDef default fill:#2e1065,color:white"
+  end
+
+  private def set_diagram_colors : Nil
+    @code += "classDef default fill:#2e1065,color:white\n"
+    @code += "classDef interface fill:#365314,color:white"
   end
 
   def save : Nil
