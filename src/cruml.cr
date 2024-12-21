8ed873a0b8cf173e77dd91013e22db4fa3207846
tamdaz <tamda.zohir.pro@gmail.com>
2024-12-21 23:55:13 +0100

    Refactor the `Cruml.run` static method.

--- src/cruml.cr
+++ src/cruml.cr
@@ -1,6 +1,8 @@
 require "./annotations"
 require "./diagram_render"
-require "./reflector"
+require "./reflection"
+require "./entities/**"
+require "./class_list"
 
 # **cruml** *(**Cr**ystal **UML**)* is a library that allows to generate a UML diagram.
 # This is useful for any Crystal projects.
@@ -24,27 +26,16 @@ require "./reflector"
 module Cruml
   VERSION = "0.1.0"
 
-  alias ClassArray = Array(Tuple(String, Symbol))
-  alias LinkSubClassArray = Array(Tuple(String, String, Symbol))
-  alias InstanceVarsArray = Array(Tuple(String, String))
-  alias MethodsArray = Array(Tuple(Symbol, String, String))
-
   # Proceed to the UML diagram generation.
   def self.run : Nil
-    reflected_classes = Cruml::Reflector.reflect_classes
-    reflected_link_subclasses = Cruml::Reflector.reflect_link_subclasses
-    reflected_instance_vars = Cruml::Reflector.reflect_instance_vars
-    reflected_methods = Cruml::Reflector.reflect_methods
-
-    if reflected_classes.empty?
-      puts "\e[31mCannot generate a UML diagram because there's no selected classes. Abort.\e[0m"
+    class_list = Cruml::Reflection.start
+    if class_list.classes.empty?
+      puts "\e[31mCannot generate UML diagram because there's no selected classes. Abort.\e[0m"
       exit 1
     end
 
     diagram_render = Cruml::DiagramRender.new(Path[::CRUML_OUT_DIR])
-    diagram_render.generate(
-      reflected_classes, reflected_link_subclasses, reflected_instance_vars, reflected_methods
-    )
+    diagram_render.generate(class_list)
     diagram_render.save
   end
 end
