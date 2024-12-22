8fb79b3bd8701ab90f4b05ea2be0a5e3acd35456
tamdaz <tamda.zohir.pro@gmail.com>
2024-12-22 19:24:17 +0100

    Increment the version to 0.2.0

--- src/cruml.cr
+++ src/cruml.cr
@@ -24,7 +24,7 @@ require "./class_list"
 # ```
 # Cruml uses macros to retrieve the names of classes and the names and types of instance variables and methods.
 module Cruml
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
 
   # Proceed to the UML diagram generation.
   def self.run : Nil
