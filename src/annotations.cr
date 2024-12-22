0a1660d00515ac98838e765273a82d8871d11c17
tamdaz <tamda.zohir.pro@gmail.com>
2024-12-22 18:29:41 +0100

    Delete `Invisible` annotation.

--- src/annotations.cr
+++ src/annotations.cr
@@ -1,13 +1,4 @@
 module Cruml::Annotation
-  # Allows to hide a class during the UML diagram generation.
-  # ```
-  # @[Cruml::Annotation::Invisible]
-  # class Secret
-  #   getter api_token : String
-  # end
-  # ```
-  annotation Invisible; end
-
   # Allows to set a class as an interface. Useful for an abstract class
   # who implements the methods signature.
   # ```
