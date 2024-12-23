d51251e56480c80ecab32aa2f8e294541d4b485d
tamdaz <tamda.zohir.pro@gmail.com>
2024-12-24 00:53:40 +0100

    Replace paranthesis by rafters thanks to tildes.

--- src/reflection.cr
+++ src/reflection.cr
@@ -17,23 +17,27 @@ module Cruml::Reflection
         {% class_type = :interface if subclass.annotation(Cruml::Annotation::AsInterface) %}
 
         class_info = Cruml::Entities::ClassInfo.new(
-          {{ subclass.name.stringify }},
+          {{ subclass.name.stringify.gsub(/[()]/, "~") }},
           {{ class_type }}
         )
 
         {% for inherit_class in subclass.subclasses %}
-          class_info.add_inherit_class({{ subclass.name.stringify }}, {{ inherit_class.name.stringify }})
+          class_info.add_inherit_class(
+            {{ subclass.name.stringify.gsub(/[()]/, "~") }},
+            {{ inherit_class.name.stringify.gsub(/[()]/, "~") }}
+          )
         {% end %}
 
         {% for instance_var in subclass.resolve.instance_vars %}
           class_info.add_instance_var(
-            {{ instance_var.name.stringify }},
+            {{ instance_var.name.stringify.gsub(/[()]/, "~") }},
             {{ instance_var.type.stringify.gsub(/[()]/, "~") }}
           )
         {% end %}
 
         {% for method in subclass.resolve.methods %}
           method_info = Cruml::Entities::MethodInfo.new(
+            {{ method.visibility }},
             {{ method.name.stringify }},
             {{ method.return_type.stringify.empty? ? "Nil" : method.return_type.stringify.gsub(/[()]/, "~") }}
           )
