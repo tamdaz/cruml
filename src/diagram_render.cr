7964a1a63b917709ec4a0d121af4a04fcace0f45
tamdaz <tamda.zohir.pro@gmail.com>
2024-12-28 21:23:01 +0100

    Improve docs to `Cruml::DiagramRender` class.

--- src/diagram_render.cr
+++ src/diagram_render.cr
@@ -1,10 +1,15 @@
+# Consists of generating a class diagram.
+# See https://mermaid.js.org/syntax/classDiagram.html
 class Cruml::DiagramRender
   @code : String = "classDiagram\n"
 
+  # The directory path must be passed as argument when instantiating this class.
   def initialize(path_dir : Path)
     @path_dir = path_dir
   end
 
+  # Generate a class diagram.
+  # A `Cruml::ClassList` class must be passed as parameter
   def generate(class_list : Cruml::ClassList) : Nil
     class_list.classes.each do |class_info|
       add_inherit_class(class_info.inherit_classes)
@@ -13,6 +18,10 @@ class Cruml::DiagramRender
     set_diagram_colors
   end
 
+  # Creates a link between the parent and child classes.
+  # If the parent class type is an interface, the arrow would look like : <|--
+  # If the parent class type is abstract or normal, the arrow would look like : <|--
+  # See https://mermaid.js.org/syntax/classDiagram.html#defining-relationship for more info.
   private def add_inherit_class(inherit_classes : Array(Tuple(String, String, Symbol))) : Nil
     inherit_classes.each do |class_name, subclass_name, class_type|
       case class_type
@@ -26,6 +35,8 @@ class Cruml::DiagramRender
     end
   end
 
+  # Creates a class with a whole set of instance variables and methods.
+  # See https://mermaid.js.org/syntax/classDiagram.html#class for more info.
   private def add_class(class_info : Cruml::Entities::ClassInfo) : Nil
     # namespace = class_info.name.split("::")[0..-2].join("")
     short_class_name = class_info.name.split("::")[-1]
@@ -35,7 +46,6 @@ class Cruml::DiagramRender
     when :abstract  then @code += "&lt;&lt;abstract&gt;&gt; #{short_class_name}\n"
     end
 
-    # @code += "namespace #{namespace} {\n"
     case class_info.type
     when :interface then @code += "  class #{short_class_name}:::interface {\n"
     when :abstract  then @code += "  class #{short_class_name} {\n"
@@ -63,15 +73,17 @@ class Cruml::DiagramRender
     end
 
     @code += "  }\n" # end class
-    # @code += "}\n"   # end namespace
   end
 
+  # Define the style properties for the class diagram.
+  # See https://mermaid.js.org/syntax/classDiagram.html#css-classes for more info.
   private def set_diagram_colors : Nil
     @code += "classDef default fill:#2e1065,color:white\n"
     @code += "classDef interface fill:#365314,color:white"
     @code += "namespaceDef default fill:#ffffff"
   end
 
+  # Save the class diagram as a HTML file.
   def save : Nil
     output = <<-HTML
     <head>
