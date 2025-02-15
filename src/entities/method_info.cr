8d8f339aa5fe4e27b90fd2a36b8285801e08ede4
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-02-15 19:16:26 +0100

    Add the `@args` instance var in the `Cruml::Entities::MethodInfo` class.

--- src/entities/method_info.cr
+++ src/entities/method_info.cr
@@ -9,6 +9,9 @@ class Cruml::Entities::MethodInfo
   # Return type of a method.
   getter return_type : String
 
+  # List of arguments of a method.
+  getter args : Array(Cruml::Entities::ArgInfo)
+
   # The scope, name and return type of a method must be passed as parameters.
   # INFO: The special `initialize` method will be placed at the first element of the list.
   # ```
@@ -18,5 +21,19 @@ class Cruml::Entities::MethodInfo
     @scope = scope
     @name = name
     @return_type = return_type
+    @args = [] of Cruml::Entities::ArgInfo
+  end
+
+  # Add an argument into the args list.
+  # ```
+  # first_name_arg = Cruml::Entities::ArgInfo.new("first_name", "String")
+  # last_name_arg = Cruml::Entities::ArgInfo.new("first_name", "String")
+  #
+  # full_name = Cruml::Entities::MethodInfo.new(:public, "full_name", "String")
+  # full_name.add_arg(first_name_arg)
+  # full_name.add_arg(last_name_arg)
+  # ```
+  def add_arg(arg : Cruml::Entities::ArgInfo)
+    @args << arg
   end
 end
