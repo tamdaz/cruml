64af9377acfea0dc29c80d84ad6ab82b708f507c
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-02-17 23:46:42 +0100

    Add the example n°6

---
+++ examples/e6_visibility_example.cr
@@ -0,0 +1,13 @@
+private class Example::E6::Visibility
+  def public_method : String
+    "This is a public method"
+  end
+
+  protected def protected_method : String
+    "This is a protected method"
+  end
+
+  private def private_method : String
+    "This is a private method"
+  end
+end
