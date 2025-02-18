3a28442d80c85a72649dd1a601fe527e416fd8c1
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-02-18 22:44:01 +0100

    Refactor the entry point file.

--- src/cruml.cr
+++ src/cruml.cr
@@ -1,41 +1,8 @@
-require "./annotations"
-require "./reflection"
-require "./class_list"
-require "./entities/**"
-require "./renders/**"
+require "./console"
 
-# **cruml** *(**Cr**ystal **UML**)* is a library that allows to generate a UML diagram.
+# **cruml** *(**Cr**ystal **UML**)* is a tool that allows to generate a UML diagram.
 # This is useful for any Crystal projects.
-#
-# ```
-# require "cruml"
-#
-# module Project
-#   # your module containing classes of all types.
-# end
-#
-# # Your project module (e.g: Project)
-# ::CRUML_FILTER_PREFIX = "Project"
-#
-# # Directory where the diagram will be saved.
-# ::CRUML_OUT_DIR = "./out/"
-#
-# Cruml.run
-# ```
-# > Cruml uses macros to retrieve reflected classes.
+# > Cruml uses the compiler API to retrieve the Crystal code.
 module Cruml
   VERSION = "0.4.0"
-
-  # Proceed to the UML diagram generation.
-  def self.run : Nil
-    class_list = Cruml::Reflection.start
-    if class_list.classes.empty?
-      puts "\e[31mUnable to generate class diagram because no class is selected. Abort.\e[0m"
-      exit 1
-    end
-
-    diagram_render = Cruml::Renders::DiagramRender.new(Path[::CRUML_OUT_DIR])
-    diagram_render.generate(class_list)
-    diagram_render.save
-  end
 end
