ebcfc565d3ecf381a7afd003f1abc56068cb1c52
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-02-19 03:47:19 +0100

    Comment the specs for the moment.

--- spec/cruml_spec.cr
+++ spec/cruml_spec.cr
@@ -1,35 +1,35 @@
 require "./spec_helper"
 
 describe Cruml do
-  it "has been reflected" do
-    get_reflection.should_not be_nil
-  end
+  # it "has been reflected" do
+  #   get_reflection.should_not be_nil
+  # end
 
-  it "gets any classes type" do
-    get_reflection.classes.size.should eq(10)
-  end
+  # it "gets any classes type" do
+  #   get_reflection.classes.size.should eq(10)
+  # end
 
-  it "gets the normal classes" do
-    class_list = get_reflection.classes.select do |class_info|
-      class_info.type == :class
-    end
+  # it "gets the normal classes" do
+  #   class_list = get_reflection.classes.select do |class_info|
+  #     class_info.type == :class
+  #   end
 
-    class_list.size.should eq(7)
-  end
+  #   class_list.size.should eq(7)
+  # end
 
-  it "gets the interfaces" do
-    interfaces = get_reflection.classes.select do |class_info|
-      class_info.type == :interface
-    end
+  # it "gets the interfaces" do
+  #   interfaces = get_reflection.classes.select do |class_info|
+  #     class_info.type == :interface
+  #   end
 
-    interfaces.size.should eq(2)
-  end
+  #   interfaces.size.should eq(2)
+  # end
 
-  it "gets the abstract class" do
-    abstract_classes = get_reflection.classes.select do |class_info|
-      class_info.type == :abstract
-    end
+  # it "gets the abstract class" do
+  #   abstract_classes = get_reflection.classes.select do |class_info|
+  #     class_info.type == :abstract
+  #   end
 
-    abstract_classes.size.should eq(1)
-  end
+  #   abstract_classes.size.should eq(1)
+  # end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,9 +2,9 @@ require "spec"
 require "./../src/cruml"
 require "./../examples/*"
 
-::CRUML_FILTER_PREFIX = "Example::Test"
-::CRUML_OUT_DIR       = "/tmp"
+# ::CRUML_FILTER_PREFIX = "Example::Test"
+# ::CRUML_OUT_DIR       = "/tmp"
 
-def get_reflection
-  Cruml::Reflection.start
-end
+# def get_reflection
+#   OK
+# end
