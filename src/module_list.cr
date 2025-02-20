30a273a9f53a8862720d4520ca66bb2b0af1e589
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-02-20 14:04:22 +0100

    Add a `Cruml::ModuleList` class.

---
+++ src/module_list.cr
@@ -0,0 +1,16 @@
+require "./entities/*"
+
+# Consists of processing a list of modules.
+class Cruml::ModuleList
+  class_getter modules = [] of Cruml::Entities::ModuleInfo
+
+  # Adds the module info to the module array.
+  def self.add(module_info : Cruml::Entities::ModuleInfo) : Nil
+    @@modules << module_info
+  end
+
+  # Find a module info by name.
+  def self.find_by_name!(module_name : String) : Cruml::Entities::ModuleInfo
+    @@modules.find! { |module_info| module_name == module_info.name }
+  end
+end
