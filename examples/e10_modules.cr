c494f23541e4e1647ea75f5f1fa541dd0cd1a417
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-02-20 21:02:04 +0100

    Change module name and class name in the `e10_modules.cr` file.

--- examples/e10_modules.cr
+++ examples/e10_modules.cr
@@ -1,16 +1,16 @@
-module ModuleOne
+module E10::ModuleOne
   def greet_one : String
     "Hello from ModuleOne"
   end
 end
 
-module ModuleTwo
+module E10::ModuleTwo
   def greet_two : String
     "Hello from ModuleTwo"
   end
 end
 
-class MyClass
+class E10::MyClass
   include ModuleOne
   include ModuleTwo
 
