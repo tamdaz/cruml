9a439c7da8446aec5aed229288ea4bddd436c3d9
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-02-22 17:20:36 +0100

    Add the `Cruml::Renders::Config` static class.

---
+++ src/renders/config.cr
@@ -0,0 +1,46 @@
+# Stores the diagram configuration.
+class Cruml::Renders::Config
+  # The theme of the diagram (`:light` or `:dark`).
+  class_property theme : Symbol = :light
+
+  # Whether to disable colors in the diagram.
+  class_property? no_color : Bool = false
+
+  # Gets the color for classes.
+  def self.class_color : String
+    @@theme == :light ? "#baa7e5" : "#2e1065"
+  end
+
+  # Gets the color for abstract classes.
+  def self.abstract_color : String
+    @@theme == :light ? "#a7e5a7" : "#365314"
+  end
+
+  # Gets the color for modules.
+  def self.module_color : String
+    @@theme == :light ? "#5ab3f4" : "#0041cc"
+  end
+
+  # Gets the theme color.
+  def self.theme_color : String
+    @@theme == :light ? "#dedede" : "#212121"
+  end
+
+  # Gets the text color.
+  def self.text_color : String
+    @@theme == :light ? "#000000" : "#ffffff"
+  end
+
+  # Generates the class definitions for the diagram.
+  def self.class_def_colors : String
+    String.build do |io|
+      [{:default, self.class_color}, {:abstract, self.abstract_color}, {:module, self.module_color}].each do |type, color|
+        io << "classDef " << type << " fill:"
+        io << (@@no_color == true ? self.theme_color : color)
+        io << ",color:" << self.text_color
+        io << ",stroke:" << (@@theme == :dark ? "#fff" : "#000")
+        io << "\n"
+      end
+    end
+  end
+end
