ecf77d7aca8018cd0e8580a07d3758c1c3f18f8c
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-02-22 16:09:13 +0100

    Add specs for the `Cruml::ClassList` and `Cruml::ModuleList` static classes.

---
+++ spec/module_list_spec.cr
@@ -0,0 +1,29 @@
+require "../src/module_list"
+require "spec"
+
+describe Cruml::ModuleList do
+  before_each { Cruml::ModuleList.clear }
+
+  describe "#add" do
+    it "adds a module to the list" do
+      module_info = Cruml::Entities::ModuleInfo.new("TestModule")
+      Cruml::ModuleList.add(module_info)
+      Cruml::ModuleList.modules.should eq([module_info])
+    end
+  end
+
+  describe "#find_by_name" do
+    it "finds a module by name" do
+      module_info = Cruml::Entities::ModuleInfo.new("TestModule")
+      Cruml::ModuleList.add(module_info)
+      found_module = Cruml::ModuleList.find_by_name!("TestModule")
+      found_module.should eq(module_info)
+    end
+
+    it "raises an error when module is not found" do
+      expect_raises(Enumerable::NotFoundError) do
+        Cruml::ModuleList.find_by_name!("NonExistentModule")
+      end
+    end
+  end
+end
