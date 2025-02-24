02366b4247a918310795c87766a353c4f575ac5b
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-02-24 20:29:41 +0100

    Refactor the `Cruml::Entities::ModuleInfo` class.

--- src/entities/module_info.cr
+++ src/entities/module_info.cr
@@ -2,13 +2,10 @@
 class Cruml::Entities::ModuleInfo
   getter name : String
   getter type : Symbol
-  getter methods : Array(Cruml::Entities::MethodInfo)
-  getter instance_vars : Array(Tuple(String, String))
+  getter methods = [] of Cruml::Entities::MethodInfo
+  getter instance_vars = [] of Tuple(String, String)
 
-  def initialize(@name : String, @type : Symbol = :normal)
-    @methods = [] of Cruml::Entities::MethodInfo
-    @instance_vars = [] of Tuple(String, String)
-  end
+  def initialize(@name : String, @type : Symbol = :normal); end
 
   # Adds a method into module.
   def add_method(method : Cruml::Entities::MethodInfo) : Nil
