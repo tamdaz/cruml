3e3f7e34069c545a52d3e8ea73f62a476efdf3b0
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-04-15 23:39:55 +0200

    Refactor `Cruml::Renders::DiagramRender`; remove `diagram_render.ecr` file.

--- src/renders/diagram_render.cr
+++ src/renders/diagram_render.cr
@@ -3,27 +3,25 @@ require "file_utils"
 require "./uml"
 
 # Consists of generating a class diagram.
-# See https://mermaid.js.org/syntax/classDiagram.html
+# See https://d2lang.com/tour/uml-classes
 class Cruml::Renders::DiagramRender
   include Cruml::Renders::UML
 
-  getter code = String::Builder.new("classDiagram\n")
+  getter code = String::Builder.new
 
   def initialize(@path_dir : String); end
 
-  # Generates the class diagram.
+  # Generates the UML class diagram.
   def generate : Nil
     generate_module_diagrams
     generate_class_diagrams
-    set_diagram_colors
   end
 
-  # Saves the class diagram as an HTML file.
+  # Saves the class diagram as a `.d2` file.
   def save : Nil
-    output = ECR.render("src/renders/diagram.ecr")
     dir = File.dirname(@path_dir)
     FileUtils.mkdir_p(dir) unless Dir.exists?(dir)
-    File.write(@path_dir, output)
+    File.write(@path_dir, code.to_s)
   rescue error : File::AccessDeniedError
     puts "An error is occured when saving the diagram into #{@path_dir}. Please retry.".colorize(:red)
     puts ("Reason of this error : " + error.to_s).colorize(:red)
