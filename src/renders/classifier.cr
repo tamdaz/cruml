8496032d15288f6eee2592dc11172fa19c04a2f5
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-04-16 16:43:12 +0200

    Add documentation for the `Cruml::Renders::Classifier` module.

--- src/renders/classifier.cr
+++ src/renders/classifier.cr
@@ -1,4 +1,7 @@
+# Classifiers consists to add classes, abstract classes, interfaces, modules and
+# namespaces into the class diagram.
 module Cruml::Renders::Classifier
+  # Adds an object into the class diagram.
   private def add_object(name : String, color : String) : Nil
     @code << '"' << name << '"' << " {\n"
     @code << Cruml::Renders::UML::INDENT << "shape: class\n"
@@ -11,30 +14,35 @@ module Cruml::Renders::Classifier
     @code << "}\n"
   end
 
+  # Adds a normal class into the class diagram.
   private def add_normal_class(name : String, &) : Nil
     add_object(name, Cruml::Renders::Config.class_color) do
       yield
     end
   end
 
+  # Adds an abstract class into the class diagram.
   private def add_abstract_class(name : String, &) : Nil
     add_object(name, Cruml::Renders::Config.abstract_color) do
       yield
     end
   end
 
+  # Adds an interface into the class diagram.
   private def add_interface(name : String, &) : Nil
     add_object(name, Cruml::Renders::Config.interface_color) do
       yield
     end
   end
 
+  # Adds a namespace into the class diagram.
   private def add_namespace(name : String, &) : Nil
     @code << '"' << name << '"' << " {\n"
     yield
     @code << "}\n"
   end
 
+  # Adds a module into the class diagram.
   private def add_module(name : String, &)
     add_object(name, Cruml::Renders::Config.class_color) do
       yield
