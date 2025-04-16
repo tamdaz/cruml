2c9bde59e7edfc13a79af11d7a85df228ad84729
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-04-16 16:34:59 +0200

    Update comments in `cruml.cr` file.

--- src/cruml.cr
+++ src/cruml.cr
@@ -2,7 +2,8 @@ require "./commands/console"
 
 # **cruml** *(**Cr**ystal **UML**)* is a tool that allows to generate an UML diagram.
 # This is useful for any Crystal projects.
-# > Cruml uses the compiler API to retrieve the Crystal code.
+#
+# > Cruml uses the Crystal Compiler API to retrieve the Crystal code.
 module Cruml
   VERSION = "0.6.2"
 end
