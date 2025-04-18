2673d228afadddbddcada9af85feafabec0df8fb
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-04-18 15:50:57 +0200

    Add E13

---
+++ examples/e13_class_vars.cr
@@ -0,0 +1,13 @@
+class E13::ClassOne
+  class_getter class_var_one : String
+  class_getter? nullable_class_var_one : String
+end
+
+class E13::ClassTwo
+  class_property class_var_two : String
+  class_property? nullable_class_var_two : String
+end
+
+class E13::ClassThree
+  class_setter class_var_three : String
+end
