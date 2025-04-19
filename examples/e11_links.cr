e90eedb0dbd6cb844593ced5955b53d4a433003d
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-04-19 21:37:31 +0200

    Add protected and private methods to display args.

--- examples/e11_links.cr
+++ examples/e11_links.cr
@@ -4,10 +4,12 @@ end
 
 class E11::G2::Sublink < E11::G1::Link
   protected def my_method : Void; end
+  protected def my_another_method_with_args(a : String, b : Int32) : Void; end
 end
 
 class E11::G3::SubSublink < E11::G2::Sublink
   private def my_another_method : Void; end
+  private def my_another_method_with_args(a : String, b : Int32) : Void; end
 end
 
 class E11::G3::AnotherSubSublink < E11::G2::Sublink
